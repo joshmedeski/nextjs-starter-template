adf52d88b4695db49e34db04bf90c4aae43ae428
Josh Medeski <josh.medeski@gmail.com>
2021-11-15 19:50:36 -0600

    lint: remove trailing semicolon from project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import React from "react";
-import Layout from "@/layout/Layout";
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import React from "react"
+import Layout from "@/layout/Layout"
 
 function NextJsStarterTemplate({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
 
-export default NextJsStarterTemplate;
+export default NextJsStarterTemplate
